f9fc19505f659afaa4fc547b60d333c6e47b3847
Andrea Natalini <andreanatalini@MacBook-Pro-di-Andrea.local>
2021-12-05 20:32:05 +0100

    modifica

--- src/components/Add.js
+++ src/components/Add.js
@@ -9,7 +9,7 @@ const Add = () => {
     event.preventDefault();
     setQuery(event.target.value);
     fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${event.target.value}`
+      `https://api.themoviedb.org/3/search/movie?api_key=441ce97aef3c684d1b44616c2ce61559&language=en-US&page=1&include_adult=false&query=${event.target.value}`
     )
       .then((res) => res.json())
       .then((data) => {
